47beb43414e80685f0c55b49acadd45d2ab3bcbe
杨涛 <tyang@dfocuspace.com>
2019-09-29 18:54:37 +0800

    spring-cloud-client~~~~

---
+++ config-client/src/main/java/com/yt/spring/cloud/config/client/ConfigClientApplication.java
@@ -0,0 +1,25 @@
+package com.yt.spring.cloud.config.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+    @Value("${eurekaUrl}")
+    private String eurekaUrl;
+
+    @GetMapping(value = "/url")
+    public String url() {
+        return eurekaUrl;
+    }
+
+}
